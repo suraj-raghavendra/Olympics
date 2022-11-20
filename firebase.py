fa71c39b98dbfbc862feda5a6eddaf82d2613427
suraj-ar <suraj.ar25@gmail.com>
2022-11-20 11:00:43 -0800

    DataNode template creation according to number of partitions done

--- firebase.py
+++ firebase.py
@@ -1,9 +1,13 @@
 from cmath import e
+from matplotlib.font_manager import json_dump
 import requests
 import json
+import sys
+import hash
 
 FIREBASE_URL = "https://olympics-62c6a-default-rtdb.firebaseio.com/"
 NAMENODE = "https://olympics-62c6a-default-rtdb.firebaseio.com/NameNode/root/"
+DATANODE = "https://olympics-62c6a-default-rtdb.firebaseio.com/DataNode/root/"
 CURRENT_DIR = "root"
 
 def init():
@@ -15,8 +19,8 @@ def init():
     r = requests.patch(NAMENODE , data = d)
     print(r.json())
 
-def show_DB():
-    data =  requests.get(FIREBASE_URL + ".json")
+def show_DB(path = None):
+    data =  requests.get(FIREBASE_URL + path + ".json")
     print(data.json())
 
 def mkdir(directory):
@@ -24,7 +28,7 @@ def mkdir(directory):
 
     levels = directory.split("/")
     PWD = NAMENODE
-    
+
     for level in levels[1:]:
         d = { level : ""}
         d = json.dumps(d)
@@ -36,7 +40,7 @@ def mkdir(directory):
         r = requests.patch(PWD + ".json", data=d)
         PWD = PWD + level + "/"
 
-    show_DB()
+    # show_DB()
 # mkdir("/user/chauhan/mahak")
 
 def cd(directory):
@@ -71,25 +75,76 @@ def ls(directory):
     else:
         print("")
 
-while(1):
+def createNameNode(filename, path, numPartition):
+    filename = filename[:-4] + "___csv"
+    filePath = path + "/" + filename
+    mkdir(filePath)
 
-    command = input(">")
-    try:
-        action, path = command.split(" ")[0], command.split(" ")[1]
-        if(action == "mkdir"):
-            mkdir(path)
-        elif(action == "ls"):
-            ls(path)
-        elif(action == "rm"):
-            rm(path)
+    #upload file metadata in the namenode
+    d = { "k" : numPartition}
+    d = json.dumps(d)
+    r = requests.patch(NAMENODE + filePath + ".json", data= d)
+
+    createDataNode(filename, numPartition)
+
+def createDataNode(fileName, numPartition):
+
+    r = requests.get(DATANODE + ".json")
+    numberOfDataNodes = len(r.json())
+    if(numberOfDataNodes):
+        if(numberOfDataNodes > numPartition):
+            return
         else:
-            break
-    except:
-        print("Invalid format")
-
-    
+            dataNodeTemplate(numPartition - numberOfDataNodes)
+    else:
+        dataNodeTemplate(numPartition)
 
 
 
-    
+def dataNodeTemplate(count):
+    """
+    creates the DataNode template;
+    if sufficient datanodes exist, nothing happens;
+    If Data nodes are not sufficient, new ones are created
+    """
+    r = requests.get(DATANODE + ".json")
+    numberOfDataNodes = len(r.json())
 
+    for i in range(count):
+        d = { "DataNode_" + str(numberOfDataNodes + 1) : ""}
+        d = json.dumps(d)
+        numberOfDataNodes += 1
+
+        r = requests.patch(DATANODE + ".json", d)
+
+
+createNameNode("cars.csv", "/user/suraj", 4)
+
+def put(path, filename, numPartition, partitionCol = None):
+
+    #TO-DO READ FILE CONTENTS
+    createNameNode(filename, path, numPartition)
+
+# while(1):
+
+#     command = input(">")
+#     if(command.split(" ")[0] == "exit"):
+#         sys.exit("Exiting ....")
+#     try:
+#         action, path = command.split(" ")[0], command.split(" ")[1]
+#         if(action == "mkdir"):
+#             mkdir(path)
+#         elif(action == "ls"):
+#             ls(path)
+#         elif(action == "rm"):
+#             rm(path)
+#         elif(action == "put"):
+#             filename = path
+#             path = command.split(" ")[2]
+#             numPartitions = command.split(" ")[3]
+#             partitionCol = command.split(" ")[4] if command.split(" ")[4] else None
+#             put(filename, path, numPartitions, partitionCol)
+#         else:
+#             break
+#     except:
+#         print("Invalid format")
